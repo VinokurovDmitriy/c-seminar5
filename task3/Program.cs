99f23d74b88da0c28ce9af0665ec54a6569a134b
dvvinokurov <dmitvitvin@gmail.com>
2022-05-01 21:34:59 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,33 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+string str = "";
+int count = 0;
+while(n > 0)
+{
+    str += "" + n % 2;
+    n /= 2;
+    count++;
+}
+
+char[] array = str.ToCharArray();
+
+void revertArray()
+{
+    for(int i = 0; i < array.Length / 2; i++)
+    {
+        char temp = array[i];
+        array[i] = array[^(i + 1)];
+        array[^(i + 1)] = temp;
+    }
+}
+
+void printRevert()
+{
+    for(int i = str.Length - 1; i >= 0; i--)
+    {
+        Console.Write(str[i]);
+    }
+    Console.WriteLine();
+}
+printRevert();
+revertArray();
+Console.WriteLine(array);
